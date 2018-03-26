ff752444af68f32a3ca290e8f5aa09caf04d4fe3
Levi Pole <leviwp48@gmail.com>
2018-03-26 16:15:49 -0700

    Working on smooth movement.

--- Mobile Apps Final Project/Assets/Scripts/Player.cs
+++ Mobile Apps Final Project/Assets/Scripts/Player.cs
@@ -7,11 +7,31 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private GameObject player;
     [SerializeField]
-    private GameObject SnowBall; 
+    private GameObject SnowBall;
+
+    public float moveTime = 0.1f;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+    private bool isMoving;
+
+    //Protected, virtual functions can be overridden by inheriting classes.
+    protected virtual void Start()
+    {
+        //Get a component reference to this object's BoxCollider2D
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        //Get a component reference to this object's Rigidbody2D
+        rb2D = GetComponent<Rigidbody2D>();
+
+        //By storing the reciprocal of the move time we can use it by multiplying instead of dividing, this is more efficient.
+        inverseMoveTime = 1f / moveTime;
+    }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             GameObject snowBallInstance =
                 Instantiate(SnowBall, player.transform.position, Quaternion.identity) as GameObject;
@@ -19,23 +39,57 @@ public class Player : MonoBehaviour {
             snowBallInstance.transform.position = new Vector2(transform.position.x + 5, transform.position.y + 2);
         }
 
-        if (Input.GetKeyDown("d"))
+        if (isMoving == false)
         {
-            double currentPos = transform.position.x;
-            currentPos = currentPos + 1;
-            player.transform.position = new Vector2((float)currentPos, transform.position.y);
+            if (Input.GetKeyDown("d"))
+            {
+                float xPos = transform.position.x + 10.0f;
+                float yPos = transform.position.y;
+                Vector3 end = new Vector3(xPos, yPos, 0.0f);
+                isMoving = true;
+                StartCoroutine(SmoothMovement(end));
+            }
+            else if (Input.GetKeyDown("a"))
+            {
+                float xPos = transform.position.x - 10.0f;
+                float yPos = transform.position.y;
+                Vector3 end = new Vector3(xPos, yPos, 0.0f);
+                isMoving = true;
+                StartCoroutine(SmoothMovement(end));
+            }
+            else if (Input.GetKeyDown("space"))
+            {
+                float xPos = transform.position.x;
+                float yPos = transform.position.y + 10.0f;
+                Vector3 end = new Vector3(xPos, yPos, 0.0f);
+                isMoving = true;
+                StartCoroutine(SmoothMovement(end));
+            }
         }
-        else if (Input.GetKeyDown("a"))
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+        //Square magnitude is used instead of magnitude because it's computationally cheaper.
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        //While that distance is greater than a very small amount (Epsilon, almost zero):
+        while (sqrRemainingDistance > float.Epsilon)
         {
-            double currentPos = transform.position.x;
-            currentPos = currentPos - 1;
-            player.transform.position = new Vector2((float)currentPos, transform.position.y);
+            //Find a new position proportionally closer to the end, based on the moveTime
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+
+            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+            rb2D.MovePosition(newPostion);
+
+            //Recalculate the remaining distance after moving.
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
+            yield return null;
         }
-        else if(Input.GetKeyDown("space"))
-        {
-            double currentPos = transform.position.y;
-            currentPos = currentPos + 2;
-            player.transform.position = new Vector2(transform.position.x, (float)currentPos);
-        }
+
+        isMoving = false;
     }
 }
