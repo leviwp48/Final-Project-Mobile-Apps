553eeffc5f1745fda7cace868057ae0d7afe9849
Levi Pole <leviwp48@gmail.com>
2018-03-27 22:27:38 -0700

    Player can now move left and right and jump off the ground

--- Mobile Apps Final Project/Assets/Scripts/Player.cs
+++ Mobile Apps Final Project/Assets/Scripts/Player.cs
@@ -2,90 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     [SerializeField]
     private GameObject player;
     [SerializeField]
-    private GameObject SnowBall;
+    private GameObject snowBall;
 
-    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+    public int moveSpeed;
+    public float jumpHeight = 1.2f;
 
-    private BoxCollider2D boxCollider;
+    private Vector2 groundCheck;
+    private float move = 0.0f;
+    private CircleCollider2D circleCollider;
     private Rigidbody2D rb2D;
-    private float inverseMoveTime;
-    private bool isMoving;
+    private bool isGrounded;
+    private bool jump;
 
     //Protected, virtual functions can be overridden by inheriting classes.
     protected virtual void Start()
     {
         //Get a component reference to this object's BoxCollider2D
-        boxCollider = GetComponent<BoxCollider2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
 
         //Get a component reference to this object's Rigidbody2D
         rb2D = GetComponent<Rigidbody2D>();
-
-        //By storing the reciprocal of the move time we can use it by multiplying instead of dividing, this is more efficient.
-        inverseMoveTime = 1f / moveTime;
     }
 
     void Update()
     {
+        //Sets move to 0 on every frame
+        move = 0.0f;
+
         if (Input.GetMouseButtonDown(0))
         {
             GameObject snowBallInstance =
-                Instantiate(SnowBall, player.transform.position, Quaternion.identity) as GameObject;
+                Instantiate(snowBall, player.transform.position, Quaternion.identity) as GameObject;
 
             snowBallInstance.transform.position = new Vector2(transform.position.x + 5, transform.position.y + 2);
         }
-
-        if (isMoving == false)
-        {
-            if (Input.GetKeyDown("d"))
-            {
-                float xPos = transform.position.x + 1.5f;
-                float yPos = transform.position.y;
-                Vector3 end = new Vector3(xPos, yPos, 0.0f);
-                isMoving = true;
-                StartCoroutine(SmoothMovement(end));
-            }
-            else if (Input.GetKeyDown("a"))
-            {
-                float xPos = transform.position.x - 1.5f;
-                float yPos = transform.position.y;
-                Vector3 end = new Vector3(xPos, yPos, 0.0f);
-                isMoving = true;
-                StartCoroutine(SmoothMovement(end));
-            }
-            else if (Input.GetKeyDown("space"))
-            {
-                float xPos = transform.position.x;
-                float yPos = transform.position.y + 1.5f;
-                Vector3 end = new Vector3(xPos, yPos, 0.0f);
-                isMoving = true;
-                StartCoroutine(SmoothMovement(end));
-            }
-        }
     }
 
-    protected IEnumerator SmoothMovement(Vector3 end)
+    //Update for physics
+    void FixedUpdate()
     {
-        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
-        //Square magnitude is used instead of magnitude because it's computationally cheaper.
-        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        //End position for player to check if they're on the ground
+        groundCheck = new Vector2(rb2D.position.x, rb2D.position.y - 0.75f);
 
-        //While that distance is greater than a very small amount (Epsilon, almost zero):
-        while (sqrRemainingDistance > float.Epsilon)
+        //Uses Linecast to see if the player is on the ground
+        isGrounded = Physics2D.Linecast(rb2D.position, groundCheck, blockingLayer);
+
+        //Checks if the jump button was pressed and if the player is on the ground
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            //Find a new position proportionally closer to the end, based on the moveTime
-            player.transform.position = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
-
-            //Recalculate the remaining distance after moving.
-            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
-            yield return null;
+            jump = true;
         }
-        isMoving = false;
+
+        //If d or a is pressed then call move function
+        move = Input.GetAxis("Horizontal");
+        Move(move);
+
+        //If player is grounded and jump is true then perform jump movement
+        if (jump)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpHeight * moveSpeed);
+            jump = false;
+        }
     }
+
+    private void Move(float moveDir)
+    {
+        rb2D.velocity = new Vector2(moveDir * moveSpeed, rb2D.velocity.y);
+    }
+
 }
