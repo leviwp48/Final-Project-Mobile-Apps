c9479b4a73a7cac7c059849ff8542bc668d6878d
Levi Pole <leviwp48@gmail.com>
2018-04-03 11:35:56 -0700

    Got rotation and aiming to work. Also made a launch function and added gravity to the ball when fired.

--- Mobile Apps Final Project/Assets/Scripts/Player.cs
+++ Mobile Apps Final Project/Assets/Scripts/Player.cs
@@ -6,130 +6,127 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject player;
-    [SerializeField]
-    private Transform snowBallPos;
+	[SerializeField]
+	private GameObject player;
+	[SerializeField]
+	private Transform snowBallPos;
 
-    public LayerMask blockingLayer;
-    public int moveSpeed;
-    public float jumpHeight = 1.2f;
-    public float maxMovement;
-    public Transform groundCheck;
-  
-    public bool isFacingLeft;
+	public LayerMask blockingLayer;
+	public int moveSpeed;
+	public float jumpHeight = 1.2f;
+	public float maxMovement;
+	public Transform groundCheck;
 
-    private Vector2 newPos;
-    private SpriteRenderer playerSprite;
-    private float movementCount;   
-    private Vector2 wallCheck;
-    private float move = 0.0f;
-    private BoxCollider2D boxCollider;
-    private Rigidbody2D rb2D;
-    private bool isGrounded;
-    private bool isWall;
-    private bool jump;
+	public bool isFacingLeft;
 
-    //Protected, virtual functions can be overridden by inheriting classes.
-    protected virtual void Start()
-    {
-        //Get a component reference to this object's BoxCollider2D
-        boxCollider = GetComponent<BoxCollider2D>();
+	private Shoot shootScript;  
+	private Vector2 newPos;
+	private SpriteRenderer playerSprite;
+	private float movementCount;   
+	private Vector2 wallCheck;
+	private float move = 0.0f;
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2D;
+	private bool isGrounded;
+	private bool isWall;
+	private bool jump;
 
-        //Get a component reference to this object's Rigidbody2D
-        rb2D = GetComponent<Rigidbody2D>();
+	//Protected, virtual functions can be overridden by inheriting classes.
+	protected virtual void Start()
+	{
+		//Get a component reference to this object's BoxCollider2D
+		boxCollider = GetComponent<BoxCollider2D>();
 
-        playerSprite = GetComponent<SpriteRenderer>();
+		//Get a component reference to this object's Rigidbody2D
+		rb2D = GetComponent<Rigidbody2D>();
 
-        isFacingLeft = true;
-    }
+		playerSprite = GetComponent<SpriteRenderer>();
 
-    void Update()
-    {
-        //Sets move to 0 on every frame
-        move = 0.0f;
+		shootScript = GameObject.Find("Main Camera").GetComponent<Shoot>();
 
-        //Checks if the jump button was pressed and if the player is on the ground
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            jump = true;
-        }
+		isFacingLeft = true;
+	}
 
-        if(isFacingLeft)
-        {           
-             newPos = new Vector2(player.transform.position.x - 3.5f, snowBallPos.position.y);
-             snowBallPos.position = newPos;
-        }
-        else if(!isFacingLeft)
-        {
-            newPos = new Vector2(player.transform.position.x + 2.5f, snowBallPos.position.y);
-            snowBallPos.position = newPos;
-        }
-    
-    }
+	void Update()
+	{
+		//Sets move to 0 on every frame
+		move = 0.0f;
 
-    //Update for physics
-    void FixedUpdate()
-    { 
-      
-        //Uses Linecast to see if the player is on the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, jumpHeight, blockingLayer);
+		//Checks if the jump button was pressed and if the player is on the ground
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			jump = true;
+		}
 
-        //If d or a is pressed then call move function
-        move = Input.GetAxis("Horizontal");
-        if (move > 0 && isFacingLeft)
-        {
-            playerSprite.flipX = true;
-            isFacingLeft = false;
-        }
-        else if (move < 0 && !isFacingLeft)
-        {
-            playerSprite.flipX = false;
-            isFacingLeft = true;
-        }
+		if(isFacingLeft)
+		{           
+			newPos = new Vector2(player.transform.position.x - 3.5f, snowBallPos.position.y);
+			snowBallPos.position = newPos;
+		}
+		else if(!isFacingLeft)
+		{
+			newPos = new Vector2(player.transform.position.x + 2.5f, snowBallPos.position.y);
+			snowBallPos.position = newPos;
+		}
 
-        wallCheck = new Vector2(rb2D.position.x + move, rb2D.position.y);
-        isWall = Physics2D.Linecast(rb2D.position, wallCheck, blockingLayer);
+	}
 
-        if(isWall)
-        {
-            return;
-        }
-        else
-        {   
-            Move(move);
-        }
-       
+	//Update for physics
+	void FixedUpdate()
+	{ 
+		if (!shootScript.isAiming) {
+			//Uses Linecast to see if the player is on the ground
+			isGrounded = Physics2D.OverlapCircle (groundCheck.position, jumpHeight, blockingLayer);
 
-        //If player is grounded and jump is true then perform jump movement
-        if (jump)
-        {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpHeight * moveSpeed);
-            jump = false;
-        }
-    }
+			//If d or a is pressed then call move function
+			move = Input.GetAxis ("Horizontal");
+			if (move > 0 && isFacingLeft) {
+				playerSprite.flipX = true;
+				isFacingLeft = false;
+			} else if (move < 0 && !isFacingLeft) {
+				playerSprite.flipX = false;
+				isFacingLeft = true;
+			}
 
-    private void Move(float moveDir)
-    {
-        movementCount = movementCount + moveDir;
-        if (Mathf.Abs(movementCount) > maxMovement)
-        {
-            if (movementCount < 0)
-            {
-                movementCount = -maxMovement;
-            }
-            else
-            {
-                movementCount = maxMovement;
-            }
+			wallCheck = new Vector2 (rb2D.position.x + move, rb2D.position.y);
+			isWall = Physics2D.Linecast (rb2D.position, wallCheck, blockingLayer);
 
-            rb2D.velocity = Vector2.zero;
-            rb2D.angularVelocity = 0.0f;
-            return;
-        }
-        else
-        {
-            rb2D.velocity = new Vector2(moveDir * moveSpeed, rb2D.velocity.y);
-        }
-    }
+			if (isWall) {
+				return;
+			} else {   
+				Move (move);
+			}
+
+
+			//If player is grounded and jump is true then perform jump movement
+			if (jump) {
+				rb2D.velocity = new Vector2 (rb2D.velocity.x, jumpHeight * moveSpeed);
+				jump = false;
+			}
+		}
+	}
+
+	private void Move(float moveDir)
+	{
+		movementCount = movementCount + moveDir;
+		if (Mathf.Abs(movementCount) > maxMovement)
+		{
+			if (movementCount < 0)
+			{
+				movementCount = -maxMovement;
+			}
+			else
+			{
+				movementCount = maxMovement;
+			}
+
+			rb2D.velocity = Vector2.zero;
+			rb2D.angularVelocity = 0.0f;
+			return;
+		}
+		else
+		{
+			rb2D.velocity = new Vector2(moveDir * moveSpeed, rb2D.velocity.y);
+		}
+	}
 }
--- Mobile Apps Final Project/Assets/Scripts/Shoot.cs
+++ Mobile Apps Final Project/Assets/Scripts/Shoot.cs
@@ -4,92 +4,136 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
 
-    public float rotateSpeed;
-    
-    [SerializeField]
-    private GameObject snowBall;
-    [SerializeField]
-    private Transform snowBallTrans;
-    [SerializeField]
-    private Transform playerTrans;
-    [SerializeField]
-    private LayerMask playerLayer;
+	public float rotateSpeed;
+	public float launchSpeed;
+	public bool isAiming;
 
+	[SerializeField]
+	private GameObject snowBall;
+	[SerializeField]
+	private Transform snowBallTrans;
+	[SerializeField]
+	private Transform playerTrans;
+	[SerializeField]
+	private LayerMask playerLayer;
 
 	private Vector3 spawnSnowBall;
-    private GameObject previousBall;
-    private Player playerScript;  
-    private Rigidbody2D rb2d;
-    private int objectCount;
-    private Vector2 mousePos;
-    
-   void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-        playerScript = GameObject.Find("Player").GetComponent<Player>();
-        objectCount = 0;
-		//spawnSnowBall = new Vector3 (snowBallTrans.position.x, snowBallTrans.position.y, 0f);
-    }
+	private GameObject previousBall;
+	private Player playerScript;  
+	private Rigidbody2D rb2d;
+	private int objectCount;
+	private Vector2 mousePos;
 
-   void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {		
-			if (objectCount == 1)
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		playerScript = GameObject.Find("Player").GetComponent<Player>();
+		objectCount = 0;
+		isAiming = false;
+
+		//spawnSnowBall = new Vector3 (snowBallTrans.position.x, snowBallTrans.position.y, 0f);
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0) && !isAiming)
+		{		
+			if (objectCount == 1) 
 			{
-				Destroy(previousBall);
+				Destroy (previousBall);
 				objectCount = 0;
 			}
-			Spawn();
+			Spawn ();
 			objectCount++;
 
 			//mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - previousBall.transform.position;
 			//float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 			//Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 			//previousBall.transform.rotation = Quaternion.Slerp (previousBall.transform.rotation, rotation, rotateSpeed * Time.deltaTime);
-            //RaycastHit2D hit = Physics2D.Linecast(mousePos, playerPosition.transform.right, playerLayer);
-            //if (hit.point != null)
-            //{
-            //Debug.Log(hit.point);
-            //spawnSnowBall = hit.point;
+			//RaycastHit2D hit = Physics2D.Linecast(mousePos, playerPosition.transform.right, playerLayer);
+			//if (hit.point != null)
+			//{
+			//Debug.Log(hit.point);
+			//spawnSnowBall = hit.point;
 
-          
-            //}
-            //else if (hit.collider == null)
-            //{
-            //    Debug.Log("was null");
-            //}
-        }
-    }
-    
-    private void Spawn()
-    {
-        if(playerScript.isFacingLeft)
-        {
-            //Vector3 offset = transform.position - playerTrans.position + snowBallTrans.position;
-           // offset = new Vector2(offset.x, offset.y);
-            //Debug.Log(offset);
-            GameObject snowBallInstance =
-                    Instantiate(snowBall, snowBallTrans.position, Quaternion.identity) as GameObject;    
-            Rigidbody2D rb2dSnow = snowBallInstance.GetComponent<Rigidbody2D>();
-            //rb2dSnow.AddForce(-snowBallInstance.transform.right * launchSpeed);
+
+			//}
+			//else if (hit.collider == null)
+			//{
+			//    Debug.Log("was null");
+			//}
+		} 
+		else if(Input.GetMouseButtonDown (0) && isAiming)
+		{
+			launch();
+		}
+		else if (isAiming) 
+		{
+			float move = Input.GetAxis ("Horizontal");
+			Debug.Log (move);
+			if (playerScript.isFacingLeft && move > 0) 
+			{
+				previousBall.transform.RotateAround (playerTrans.position, Vector3.forward, -rotateSpeed * Time.deltaTime);
+			} 
+			else if(playerScript.isFacingLeft && move < 0)
+			{
+				previousBall.transform.RotateAround (playerTrans.position, Vector3.forward, rotateSpeed * Time.deltaTime);
+			}
+			else if (!playerScript.isFacingLeft && move > 0) 
+			{
+				previousBall.transform.RotateAround (playerTrans.position, Vector3.forward, -rotateSpeed * Time.deltaTime);
+			} 
+			else if (!playerScript.isFacingLeft && move < 0) 				
+			{
+				previousBall.transform.RotateAround (playerTrans.position, Vector3.forward, rotateSpeed * Time.deltaTime);
+			}
+		}
+	}
+
+	private void Spawn()
+	{
+		if(playerScript.isFacingLeft)
+		{
+			//Vector3 offset = transform.position - playerTrans.position + snowBallTrans.position;
+			// offset = new Vector2(offset.x, offset.y);
+			//Debug.Log(offset);
+			GameObject snowBallInstance =
+				Instantiate(snowBall, snowBallTrans.position, Quaternion.identity) as GameObject;    
+			Rigidbody2D rb2dSnow = snowBallInstance.GetComponent<Rigidbody2D>();
+			//rb2dSnow.AddForce(-snowBallInstance.transform.right * launchSpeed);
 			rb2dSnow.gravityScale = 0.0f;
-            previousBall = snowBallInstance;
+			previousBall = snowBallInstance;
+			isAiming = true;
+		}
+		else
+		{
+     		// Vector2 right = new Vector2(spawnSnowBall.x, spawnSnowBall.y);
+			// Debug.Log(right);
 
-        }
-        else
-        {
-
-           // Vector2 right = new Vector2(spawnSnowBall.x, spawnSnowBall.y);
-           // Debug.Log(right);
-
-            GameObject snowBallInstance =
-                    Instantiate(snowBall, snowBallTrans.position, Quaternion.identity) as GameObject;
-            Rigidbody2D rb2dSnow = snowBallInstance.GetComponent<Rigidbody2D>();
-            //rb2dSnow.AddForce(snowBallInstance.transform.right * launchSpeed);
+			GameObject snowBallInstance =
+				Instantiate(snowBall, snowBallTrans.position + Vector3.right, Quaternion.identity) as GameObject;
+			Rigidbody2D rb2dSnow = snowBallInstance.GetComponent<Rigidbody2D>();
+			//rb2dSnow.AddForce(snowBallInstance.transform.right * launchSpeed);
 			rb2dSnow.gravityScale = 0.0f;
+			previousBall = snowBallInstance;
+			isAiming = true;
+		}
+	}
 
-            previousBall = snowBallInstance;
-        }
-    }
+	private void launch()
+	{
+		if (playerScript.isFacingLeft) {
+			Rigidbody2D rb2dSnow = previousBall.GetComponent<Rigidbody2D> ();
+			rb2dSnow.gravityScale = 1f;
+			rb2dSnow.AddForce (-previousBall.transform.right * launchSpeed);
+			isAiming = false;
+		} 
+		else {
+			Rigidbody2D rb2dSnow = previousBall.GetComponent<Rigidbody2D> ();
+			rb2dSnow.gravityScale = 1f;
+			rb2dSnow.AddForce (previousBall.transform.right * launchSpeed);
+			isAiming = false;
+			}
+		}
+
 }
