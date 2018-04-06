aeb9fc245c25b5c16b5c081c0a5b2b25b26574f9
Levi Pole <leviwp48@gmail.com>
2018-04-05 23:04:51 -0700

    Deleted Mobile Apps Final Project folder and replaced it with a fresh Tactical RPG folder that holds the same assets.

---
+++ Tactical RPG/Assets/Scripts/ShootCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCollision : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject currBall;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Floor") {
+			Destroy (this.gameObject, 0.5f);		
+		}
+		if (coll.gameObject.tag == "Player2") {
+			Destroy (this.gameObject, 0.5f);		
+
+		}
+	}
+}
